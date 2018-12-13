b6fee88710e6382e7703320058cde7bd173bf2d9
github username <github email address>
2018-12-13 01:09:16 -0500

    Done.

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,18 @@
 class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
+
+
   end
 
   def show
     @artist = Artist.find(params[:id])
+  end
+
+  def songs_index
+    @artist  = Artist.find(params[:id])
+    @songs = @artist.songs
+    render template: 'songs/index'
   end
 
   def new
